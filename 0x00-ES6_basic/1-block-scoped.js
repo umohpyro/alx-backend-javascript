a23a0e0bb79ed44c67c177c1f8b8ba57bf281ff2
Umoh Andem <andemumoh@gmail.com>
2023-04-17 23:41:06 +0100

    Task 1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
-export default function taskBlock (trueOrFalse) {
-  var task = false;
-  var task2 = true;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    return [task, task2];
   }
-
   return [task, task2];
 }
