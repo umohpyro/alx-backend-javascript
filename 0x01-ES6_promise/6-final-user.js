8b42598b2bd1103efb9d3be750d0d3871ae8dea3
Umoh Andem <andemumoh@gmail.com>
2023-04-19 15:02:28 +0100

    Task 6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const userData = signUpUser(firstName, lastName);
   const photoData = uploadPhoto(fileName);
 
-  const values_1 = await Promise.allSettled([userData, photoData,]);
-  const response = [];
-  values_1.map((item) => {
-    //console.log(item.value);
-    if (item.status === 'fulfilled') {
-      response.push({ status: item.status, value: item.value });
-      //console.log(response);
-    } else {
-      response.push({ status: item.status, value: `${item.reason}` });
-     // console.log(response);
-    }
+  return Promise.allSettled([userData, photoData,]).then((values) => {
+    const response = [];
+    values.map((item) => {
+      //console.log(item.value);
+      if (item.status === 'fulfilled') {
+        response.push({ status: item.status, value: item.value });
+       // console.log(response);
+      } else {
+        response.push({ status: item.status, value: `${item.reason}` });
+      //  console.log(response);
+      }
+    });
+    return response;
   });
-  return response;
 }
