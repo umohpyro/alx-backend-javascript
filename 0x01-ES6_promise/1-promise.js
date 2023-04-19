3cd4ba1d429f75f07b63732c0dfb9dcb0ef7579a
Umoh Andem <andemumoh@gmail.com>
2023-04-19 10:46:31 +0100

    Task 1. Don't make a promise...if you know you can't keep it.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI (success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
