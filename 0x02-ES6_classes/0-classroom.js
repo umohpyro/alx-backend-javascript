75b353d953aaf19028d54e9f513198e663b50e45
Umoh Andem <andemumoh@gmail.com>
2023-04-20 12:11:38 +0100

    Task 0. You used to attend a place like this at some point

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,15 @@
+export default class ClassRoom{
+    constructor(maxStudentsSize){
+        this._maxStudentsSize = maxStudentsSize;
+    }
+    get maxStudentsSize(){
+        return this._maxStudentsSize;
+    }
+    set maxStudentsSize(newSize){
+        if (typeof newSize === "number"){
+        this._maxStudentsSize = newSize;
+        } else {
+        console.log("Size must be a number");
+        }
+    }
+}
