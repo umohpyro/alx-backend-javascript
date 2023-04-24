118ec437b418d19d6b21cb3b2e3cb86d1829b7eb
Umoh Andem <andemumoh@gmail.com>
2023-04-24 22:15:26 +0100

    Task 8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,19 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string') {
-    throw Error('startString must be a string');
+  const list = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
   }
-  for (const value of set) {
-    if (typeof value !== 'string') {
-      throw Error('set must only contain strings');
-    }
-    if (
-      value.startsWith(startString)
-      && startString.length > 0
-      && startString !== undefined
-    ) {
-      return [...set]
-        .filter((value) => value.startsWith(startString))
-        .map((value) => value.slice(startString.length))
-        .join('-');
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
     }
   }
-  return '';
+
+  return list.join('-');
 }
