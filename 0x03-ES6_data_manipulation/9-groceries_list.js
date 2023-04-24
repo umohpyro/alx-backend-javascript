b9c333d63705270fe516c3645d813187a138577b
Umoh Andem <andemumoh@gmail.com>
2023-04-24 22:28:27 +0100

    Task 9. Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+export default function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+
+  // for (const [key, value] of groceries) {
+  //     console.log(`groceries: ${key}, quantity: ${value}`);
+  // }
+
+  return groceries;
+}
