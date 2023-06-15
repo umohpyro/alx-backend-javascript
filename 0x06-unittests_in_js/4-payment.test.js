27bd3bd33a2527b23a52bdd055f9e250f5e4f488
Umoh Andem <andemumoh@gmail.com>
2023-06-15 10:15:24 +0100

    Task 4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,11 +5,12 @@ const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', function() {
   beforeEach(function() {
-    sinon.spy(Utils, 'calculateNumber');
+    sinon.spy(console, "log");
+    sinon.stub(Utils, 'calculateNumber').returns(10);
   });
   afterEach(function() {
     sinon.restore();
@@ -26,5 +27,9 @@ describe('sendPaymentRequestToApi', function() {
     expect(Utils.calculateNumber.firstCall.args[0]).to.equal("SUM");
     // Check params
     expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    // Check console call count
+    expect(console.log.callCount).to.be.equal(1);
+    // Check console param
+    expect(console.log.calledWith('The total is: 10')).to.be.true;
   });
 });
